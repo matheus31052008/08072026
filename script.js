d20c6ba7db36a113e80331b805a0474352401c3c
matheus <pereira.matheus31@escola.pr.gov.br>
2026-07-08 16:05:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+            // tamanhoSenha = tamanhoSenha + 1;
+            tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
